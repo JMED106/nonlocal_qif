da52d2be4b982e358116735fd0f30581b08761af
JMED106 <josemesnaola@gmail.com>
2014-05-07 11:06:42 +0200

    Modifying structures

--- simulation/common.h
+++ simulation/common.h
@@ -2,36 +2,20 @@
 #define __COMMON__
 
 typedef struct {
-  int id;			/* Id of the oscillator */
-  int id_rand;
   double v;
-  double ph;
-  double th;			/* Phase of the osc. */
-  double w;			/* w of the osc */
-  double vp;			/* peak potential i */
-  double vr;			/* reset potential i */
-  double J;
+
   double eta;
-  double g;
-  double V0;
+
   double r;			/* Firing Rate saved in each oscillator */
-  double r2;
   double spike;			/* Has the oscillator spiked? */
-  double spike2;
   int tr;			/* How many steps ago has the oscillator spiked? */
-  int tr2;
-  int total_spikes;			/* Total number of spike of this oscillator */
+  int total_spikes;	        /* Total number of spike of this oscillator */
   int global_s1;
-  int global_s2;
+
   int N;			/* Total number of oscillators (redundant) */
   double FR;
   int pert;
-
-  double rh;			/* r and v of the firing rate equations. */
-  double vh;
-  double rh2, vh2; 		/* Copy of the previous ones */
-
-} t_th;
+} t_qif;
 
 typedef struct {
   int init_dist;		/* Type of initial distribution */
@@ -39,20 +23,12 @@ typedef struct {
   double vp;			/* Peak potential */
 
   double J;			/* J */
-  double J_sigma;		/* Dispersion of the J */
+  double DJ;			/* Dispersion of the J */
   int J_dist;			/* Type of distribution of J */
 
   double eta;			/* External current */
-  double eta_sigma;		/* Dispersion of the external current */
+  double Deta;		        /* Dispersion of the external current */
   int eta_dist;			/* Type of distribution of eta */
-
-  double g;			/* g */
-  double g_sigma;		/* Dispersion of the g */
-  int g_dist;			/* Type of distribution of g */
-
-  double v0;			/* v0 */
-  double v0_sigma;		/* Dispersion of the v0 */
-  int v0_dist;			/* Type of distribution of v0 */
 
   int N;			/* Number of neurons */
   double TT;			/* Total time of the simulation */
@@ -63,11 +39,9 @@ typedef struct {
   int scan;			/* counter */
   int scan_max;
 
-  int variable;			/* Variable to scan (...?) */
-  double var_value;
-  double min_x;			/* Minimum value of the variable to scan */
-  double max_x;			/* Maximum value of the variable to scan */
-  double dx;			/* Increment of the value in each simulation */
+  double min;	                /* Minimum value of the variable to scan */
+  double max;			/* Maximum value of the variable to scan */
+  double step;			/* Increment of the value in each simulation */
   int MaxDim;			/* Maximum dimension of the raster plot */
   int disable_raster;
 
@@ -78,17 +52,19 @@ typedef struct {
   int pert;
   double pert_amplitude;        /* Amplitude of the perturbation QIF */
   double perturbation_FR;	/* FR *** Amplitude of the perturbation FR*/
+
+  /* Related to the non-localized system */
+  int l;			/* Number of clusters (columns) */
+  double J0,J1,J2;		/* Amplitudes of the modes of the coupling function */
+  int ring;			/* Boolean to determine periodic boundaries */
 } t_data;
 
 
 typedef struct {
-  double r;
-  double v;
+  double r, r2;
+  double v, v2;
 } t_FR;
 
-typedef struct {
-  int l;			/* Number of functional columns */
-} t_system;
 
 #ifndef GSL_SIGN
 #define GSL_SIGN(x) ((x) >= 0 ? 1: -1)
