c90976b813a32136fdb671382d8619ac32ba708c
JMED106 <josemesnaola@gmail.com>
2014-06-03 11:36:13 +0200

    Added neural field library

---
+++ simulation/code/src/field.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "field.h"
+
+void N_F(double *J,double *rx,int i,  t_data d) {
+  int j;
+  J[i] = 0.0;
+/* #pragma omp parallel pivate(j) */
+  for(j=1 ;j<d.l ;j++ ) 
+    J[i] += J_F(d,i,j,rx);
+  J[i] /= d.l;
+}
+
+
+double J_F(t_data d, int i, int j, double *rx) {  
+  return (d.J0 + 2.0*d.J1*cos((i-j)*d.dx))*rx[j];
+}
---
+++ simulation/code/src/field.h
@@ -0,0 +1,8 @@
+#ifndef __FIELD__h__
+#define __FIELD__h__
+
+#include "common.h" 
+
+void N_F(double *J,double *rx, int i,  t_data d);
+double J_F(t_data d, int i, int j, double *rx);
+#endif
